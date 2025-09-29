ec573bd80ba8f13ad5182a7f17d850e89cd6eb94
Kris465 <nataza7394@gmail.com>
2025-09-29 15:15:02 +0300

    Maklusov

---
+++ block1/task3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите своё родное число: ");
+String inputString = Console.ReadLine();
+int myNumber;
+Int32.TryParse(inputString, out myNumber);
+Console.WriteLine("Вы ввели число: ");
+Console.WriteLine(myNumber);
