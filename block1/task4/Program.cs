6047736dcd4934924c13def9bf4abbbd4953bc78
Kris465 <nataza7394@gmail.com>
2025-09-29 15:30:34 +0300

    Попов Георгий 4 дз

---
+++ block1/task4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число: ");
+String inputString = Console.ReadLine();
+int myNumber;
+Int32.TryParse(inputString, out myNumber);
+Console.WriteLine(myNumber);
+Console.WriteLine("вот какое число.Вы ввели");
