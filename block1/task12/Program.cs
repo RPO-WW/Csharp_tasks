d6a386dba89af45678d0956fe073b31e67c9a2f2
Durak2000 <fetisovtimur0@gmail.com>
2025-09-30 11:55:31 +0300

    Maklusov

---
+++ block1/task12/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число x: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число y: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число t: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число v: ");
+int num4 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"а) 5 10   б) 100 {num3}   в) {num1} 25");
+Console.WriteLine($"   7 см      1949 {num4}     {num1} {num2}");
