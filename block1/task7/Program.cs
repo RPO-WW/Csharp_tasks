82a9e23e41fcce88fc650f3b9f52afb23a014dc1
Durak2000 <fetisovtimur0@gmail.com>
2025-09-30 10:27:39 +0300

    Maniraki

---
+++ block1/task7/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    Console.Write("Введите нулевое число: ");
+    int num0 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите первое число: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите второе число: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"Введенное число: {num0}  {num1}  {num2}");
+}
+catch
+{
+    Console.WriteLine("ОШИБКА!!!");
+}
