6a3b7f08f980ac999b7e061bcca94ac5887a5843
Durak2000 <fetisovtimur0@gmail.com>
2025-09-30 11:41:21 +0300

    соломахин

---
+++ block1/task11/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите четвертое число: ");
+int num4 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+Console.WriteLine(num3);
+Console.WriteLine(num4);
---
+++ block1/task9/Program.cs
@@ -0,0 +1,5 @@
+﻿int num1 = 50;
+int num2 = 10;
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
