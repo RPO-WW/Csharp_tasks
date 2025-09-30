ffdeb3b5993bcb25e795b8df66b89f967cbdc27c
Durak2000 <fetisovtimur0@gmail.com>
2025-09-30 10:57:15 +0300

    kirill

---
+++ block1/task8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите четвертое число: ");
+int num4 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{num1} {num2} {num3} {num4}");
