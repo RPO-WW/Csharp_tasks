6a4d383431256acd24a3b9c45107a2b7a737cad5
brother <hacker@gmail.com>
2025-10-01 15:36:27 +0300

    Манираки

---
+++ block1/task23/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите значение a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+double a_0 = Math.Pow(a, 2) + 1;
+double task = (Math.Pow(a, 2) + 10) / Math.Pow(a_0, 0.5);
+Console.WriteLine($"y = {Math.Round(task, 2)}");
