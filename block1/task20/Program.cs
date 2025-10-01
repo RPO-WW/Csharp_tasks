f0aecd26ca2c17f20592f753ce53e1d7c24369d1
brother <hacker@gmail.com>
2025-10-01 13:49:39 +0300

    Манираки

---
+++ block1/task20/Program.cs
@@ -0,0 +1,20 @@
+﻿try
+{
+    int s0 = 14;
+    int k0 = -3;
+    int d0 = s0 + 1;
+    s0 = d0;
+    k0 = 2 * s0;
+    Console.WriteLine($"а) {s0} {k0} {d0}");
+
+    int s1 = 0;
+    int k1 = 30;
+    int d1 = k1 - 5;
+    k1 = 2 * d1;
+    s1 = k1 - 100;
+    Console.WriteLine($"б) {s1} {k1} {d1}");
+}
+catch
+{
+    Console.WriteLine("Ошибка");
+}
