40886d4db2efbc97afbace7f9bc83f69faf7b229
Durak2000 <fetisovtimur0@gmail.com>
2025-10-02 12:59:41 +0300

    соломахин

---
+++ block1/task26/Program.cs
@@ -0,0 +1,18 @@
+﻿using system
+
+class Program
+{
+    static void Main()
+    {
+        Console.write("Введите радиус окркжности: ");
+        double radius = CalculateDiameter(radius);
+
+        Console.WriteLine($"Димаетр окружности: {diameter}");
+    }
+
+    static double
+CalculateDiameter(double radius)
+    {
+        return 2 * radius;
+    }
+}
