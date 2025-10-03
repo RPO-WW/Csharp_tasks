968c98a7aaf43ca1476ea04b87409b160c5eafbf
Kris465 <nataza7394@gmail.com>
2025-10-03 11:31:40 +0300

    ksusha

---
+++ block1/task29/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите радиус окружности: ");
+
+        if (double.TryParse(Console.ReadLine(), out double radius))
+        {
+            if (radius < 0)
+            {
+                Console.WriteLine("Радиус не может быть отрицательным");
+                return;
+            }
+
+            double circumference = 2 * Math.PI * radius;
+            double area = Math.PI * radius * radius;
+
+            Console.WriteLine($"Длина окружности: {circumference}");
+            Console.WriteLine($"Площадь круга: {area}");
+        }
+        else
+        {
+            Console.WriteLine("Неккоректный ввод. Пожалуйста , введите число");
+        }
+    }
+}
