52a77ac8a8982c672bd0fa492834b96bafea5b0f
Kris465 <nataza7394@gmail.com>
2025-10-03 11:24:53 +0300

    vita

---
+++ block1/task28/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите длину ребра куба: ");
+        double edgeLenght = Convert.ToDouble(Console.ReadLine());
+
+        double volume = Math.Pow(edgeLenght, 3);
+
+        double lateralSurfaceArea = 4 * Math.Pow(edgeLenght, 2);
+
+        Console.WriteLine($"Обьем куба: {volume:F2}");
+        Console.WriteLine($"Площадь боковой поверхности: {lateralSurfaceArea}");
+    }
+}
