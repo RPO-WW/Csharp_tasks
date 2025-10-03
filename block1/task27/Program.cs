86e6bbec6d6d383b591eb91d9105bdbc5c8f01a2
Kris465 <nataza7394@gmail.com>
2025-10-03 09:58:33 +0300

    сделал дз 27

---
+++ block1/task27/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите высоту над землей(в километрах):");
+        double heightKm = double.Parse(Console.ReadLine());
+
+        double distance = CalculateHorizonDistance(heightKm);
+        Console.WriteLine($"расстояние до горизонта: {distance:F2} км");
+    }
+
+    static double CalculateHorizonDistance(double heightKm)
+    {
+        const double R = 6350.0;
+        return Math.Sqrt(2 * R * heightKm + Math.Pow(heightKm, 2));
+    }
+}
