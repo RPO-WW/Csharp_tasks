b0722827f9ed089069428321ec519e2a32eedfb3
Kris465 <nataza7394@gmail.com>
2025-10-07 10:17:10 +0300

    соломахин

---
+++ block1/task37/Program.cs
@@ -0,0 +1,9 @@
+﻿ using System; 
+class New_Project  
+{ public static void Main()  
+{ double a, b, c; Console.Write("a b: "); 
+  var line = Console.ReadLine().Split(" "); 
+  a = double.Parse(line[0]); b = double.Parse(line[1]); 
+  c = Math.Sqrt(a * a + b * b); 
+  Console.WriteLine("Гипотенуза: " + c); 
+  Console.WriteLine("Периметр:   " + (a + b + c)); } } 
