4809470dd70c84e3e936b3adf022f3e8b1a2fa0c
Kris465 <nataza7394@gmail.com>
2025-10-07 10:00:27 +0300

    fetisov

---
+++ block1/task35/Program.cs
@@ -0,0 +1,8 @@
+﻿double katet1 = 20;
+double katet2 = 30;
+
+double gipotenuz = Math.Pow(katet1, 2) + Math.Pow(katet2, 2);
+
+double otvet = Math.Pow(gipotenuz, 0.5);
+
+Console.WriteLine(Math.Round(otvet, 2));
