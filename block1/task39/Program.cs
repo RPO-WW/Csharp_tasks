efc04973a9e3754c5d93c99f572105e0bfe08858
Kris465 <nataza7394@gmail.com>
2025-10-07 11:56:06 +0300

    Манираки

---
+++ block1/task39/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Ввдеите число x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ввдеите число y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+double z = Math.Round(x + (2 + y) / Math.Pow(x, 2) / (y + (1 / Math.Pow(Math.Pow(x, 2) + 10, 0.5))), 2);
+double q = Math.Round(2.8 * Math.Sin(x) + Math.Abs(y), 2);
+Console.WriteLine($"z = {z}, q = {q}");
