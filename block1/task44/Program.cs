29dbe47aca6a61b1690e2934280a9927fe497d1e
Kris465 <nataza7394@gmail.com>
2025-10-08 10:21:09 +0300

    popov

---
+++ block1/task44/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Rectangle
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длину первой стороны прямоугольника (a):");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите длину второй стороны прямоугольника (b):");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double perimeter = 2 * (a + b);
+        Console.WriteLine($"Периметр прямоугольника: {perimeter}");
+
+        double diagonal = Math.Sqrt(a * a + b * b);
+        Console.WriteLine($"Длина диагонали прямоугольника: {diagonal}");
+    }
+}
