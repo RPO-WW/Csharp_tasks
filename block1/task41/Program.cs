b094dd33cd819b00208daef21ac1b7163cc52812
Kris465 <nataza7394@gmail.com>
2025-10-08 09:45:09 +0300

    соломахин

---
+++ block1/task41/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Ввдеите число e: ");
+double e = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ввдеите число f: ");
+double f = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ввдеите число g: ");
+double g = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ввдеите число h: ");
+double h = Convert.ToDouble(Console.ReadLine());
+
+double a = Math.Round(Math.Pow(Math.Abs(e - (3 / f)), 0.33) + g, 2);
+double b = Math.Round(Math.Sin(e) + Math.Pow(Math.Cos(h), 2), 2);
+double c = Math.Round((33 * g) / (e * f - 3), 2);
+Console.WriteLine($"a = {a}, b = {b}, c = {c}");
