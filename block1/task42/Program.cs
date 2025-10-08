3a53f305a2a6acb86ad2320835e19f84dab18b1b
Kris465 <nataza7394@gmail.com>
2025-10-08 09:56:41 +0300

    Кирилл

---
+++ block1/task42/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Ввдеите число e: ");
+double e = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ввдеите число f: ");
+double f = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ввдеите число g: ");
+double g = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ввдеите число h: ");
+double h = Convert.ToDouble(Console.ReadLine());
+
+double a = Math.Round((e + (f / 2)) / 3, 2);
+double b = Math.Round(Math.Abs(Math.Pow(h, 2) - g), 2);
+double c = Math.Round(Math.Pow(Math.Pow(g - h, 2) - 3 * Math.Sin(e), 0.5), 2);
+Console.WriteLine($"a = {a}, b = {b}, c = {c}");
