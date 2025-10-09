2830f4cfac0051e556a5c2a744113766f07f9f00
Kris465 <nataza7394@gmail.com>
2025-10-09 10:13:02 +0300

    Манираки

---
+++ block1/task48/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("Ввдеите одно основание: ");
+int osn1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ввдеите второе основание: ");
+int osn2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ввдеите высоту: ");
+int h = Convert.ToInt32(Console.ReadLine());
+
+
+double c = Math.Pow(Math.Pow(h, 2) + Math.Pow((osn1 - osn2) / 2, 2), 0.5);
+double p = osn1 + osn2 + 2 * c;
+
+Console.WriteLine($"Периметр: {p}");
