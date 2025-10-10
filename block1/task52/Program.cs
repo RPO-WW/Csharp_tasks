ade432ea668057b85af05af5a9b61096e34c14f2
Kris465 <nataza7394@gmail.com>
2025-10-10 13:17:37 +0300

    соломахин

--- block1/task52/Program.cs
+++ block1/task52/Program.cs
@@ -1,32 +1,35 @@
-﻿using system;
+﻿using System;
 
-
-class Program
+public class Program
 {
-    static void Main()
+    public static void Main()
     {
         Console.WriteLine("Введите стоимость 1 кг конфет: ");
         double candyPrice = double.Parse(Console.ReadLine());
-
+        
         Console.WriteLine("Введите стоимость 1 кг печенья: ");
         double cookiePrice = double.Parse(Console.ReadLine());
-
+        
         Console.WriteLine("Введите стоимость 1 кг яблок: ");
         double applePrice = double.Parse(Console.ReadLine());
-
+        
         Console.WriteLine("Введите количество кг конфет: ");
-        double candyWeight = double.Parse(Console.ReadLine());
-
+        double x = double.Parse(Console.ReadLine());
+        
         Console.WriteLine("Введите количество кг печенья: ");
-        double cookieWeight = double.Parse(Console.ReadLine());
-
+        double y = double.Parse(Console.ReadLine());
+        
         Console.WriteLine("Введите количество кг яблок: ");
-        double appleWeight = double.Parse(Console.ReadLine());
+        double z = double.Parse(Console.ReadLine());
+        
 
-        double totalCost = (candyPrice * candyWeight) + (cookiePrice * CookieWeight) + (applePrice * applePriceWeight);
-
-        Console.WriteLine($"Общая стоимость покупки: {totalCost:F2} руб.");
-
+        double totalCost = (candyPrice * x) + (cookiePrice * y) + (applePrice * z);
+        
+        Console.WriteLine("\n=== ЧЕК ПОКУПКИ ===");
+        Console.WriteLine($"Конфеты: {x} кг × {candyPrice:F2} руб = {candyPrice * x:F2} руб");
+        Console.WriteLine($"Печенье: {y} кг × {cookiePrice:F2} руб = {cookiePrice * y:F2} руб");
+        Console.WriteLine($"Яблоки:  {z} кг × {applePrice:F2} руб = {applePrice * z:F2} руб");
+        Console.WriteLine("-------------------");
+        Console.WriteLine($"ОБЩАЯ СТОИМОСТЬ: {totalCost:F2} руб");
     }
-
 }
