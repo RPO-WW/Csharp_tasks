8756475d55f0e51f943dc8275e8f9177a23eaa3d
Kris465 <nataza7394@gmail.com>
2025-10-10 12:07:56 +0300

    соломахин

---
+++ block1/task52/Program.cs
@@ -0,0 +1,32 @@
+﻿using system;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите стоимость 1 кг конфет: ");
+        double candyPrice = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите стоимость 1 кг печенья: ");
+        double cookiePrice = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите стоимость 1 кг яблок: ");
+        double applePrice = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите количество кг конфет: ");
+        double candyWeight = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите количество кг печенья: ");
+        double cookieWeight = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите количество кг яблок: ");
+        double appleWeight = double.Parse(Console.ReadLine());
+
+        double totalCost = (candyPrice * candyWeight) + (cookiePrice * CookieWeight) + (applePrice * applePriceWeight);
+
+        Console.WriteLine($"Общая стоимость покупки: {totalCost:F2} руб.");
+
+    }
+
+}
