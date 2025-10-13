bb78149126b13636cf28ea727ef8bc964e193de2
Kris465 <nataza7394@gmail.com>
2025-10-13 13:18:20 +0300

    popov

---
+++ Block2/task4/Program.cs
@@ -0,0 +1,6 @@
+﻿
+int distanceInMeters = 5678;
+
+int distanceInKilometers = distanceInMeters / 1000;
+
+Console.WriteLine($"В {distanceInMeters} метрах содержится {distanceInKilometers} полных километров.");
