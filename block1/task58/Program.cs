cf82d488e9ee91529c6d097e004c1e5f10bc04eb
Kris465 <nataza7394@gmail.com>
2025-10-13 10:05:02 +0300

    linda

---
+++ block1/task58/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        long population;
+        double area;
+        Console.Write("Введите количество жителей: ");
+        while (!long.TryParse(Console.ReadLine(), out population) || population < 0)
+        {
+            Console.WriteLine("Введите число жителей неотрицательное: ");
+        }
+        Console.WriteLine("Введите площадь в кв км");
+        while (!double.TryParse(Console.ReadLine(), out area) || area <= 0)
+        {
+            Console.WriteLine("Площадь должна быть больше нуля: ");
+        }
+        double populationDensity = population / area;
+        Console.WriteLine($"Плотность населения на кв метр равна: {populationDensity} жителей на кв км");
+    }
+}
