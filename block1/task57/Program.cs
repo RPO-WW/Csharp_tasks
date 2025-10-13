a56cfe1f12feb6e9188d44371a50ddb6d0b6fc10
Kris465 <nataza7394@gmail.com>
2025-10-13 09:28:03 +0300

    ksusha

---
+++ block1/task57/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите температуру в градусах Цельсия: ");
+
+        if (!double.TryParse(Console.ReadLine(), out double celsius))
+        {
+            Console.WriteLine("Ошибка: введено некорректное значение.");
+            return;
+        }
+
+        double fahrenheit = celsius * 1.8 + 32;
+
+        double kelvin = celsius + 273.15;
+
+        Console.WriteLine($"Температура по шкале Фаренгейта: {fahrenheit}");
+        Console.WriteLine($"Температура по шкале Кельвина: {kelvin}");
+    }
+}
