47228506f4d86c310fc2f7bb99372d6a84e8883b
Kris465 <nataza7394@gmail.com>
2025-10-15 11:21:39 +0300

    георгий

---
+++ Block2/task10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите двузначное число: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        if (number < 10 || number > 99)
+        {
+            Console.WriteLine("Ошибка! Введите двузначное число (10-99).");
+            return;
+        }
+        
+        int tens = number / 10;
+        
+        int units = number % 10;
+        
+        int sum = tens + units;
+        
+        int product = tens * units;
+        
+        Console.WriteLine($"Исходное число: {number}");
+        Console.WriteLine($"а) Число десятков: {tens}");
+        Console.WriteLine($"б) Число единиц: {units}");
+        Console.WriteLine($"в) Сумма цифр: {sum}");
+        Console.WriteLine($"г) Произведение цифр: {product}");
+    }
+}
