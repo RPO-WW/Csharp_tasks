815739d50cd7b3530c97a410f0107b9553afcd59
Kris465 <nataza7394@gmail.com>
2025-10-15 13:44:09 +0300

    Манираки

---
+++ Block2/task15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ввдеите трёхзначное число: ");
+int originalNumber = Convert.ToInt32(Console.ReadLine());
+
+int lastDigit = originalNumber % 10;
+int firstDigit = originalNumber / 100;
+int middleDigit = (originalNumber / 10) % 10;
+
+int newNumber = lastDigit * 100 + firstDigit * 10 + middleDigit; 
+
+Console.WriteLine($"Исходное число: {originalNumber}");
+Console.WriteLine($"Полученное число: {newNumber}"); 
