549831c96ecc0a75d247dd036de4b78be63c60c2
Kris465 <nataza7394@gmail.com>
2025-10-15 13:10:40 +0300

    кирилл

---
+++ Block2/task13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // а) 2 кг 13 17
+        Console.WriteLine("2 кг 13 17");
+
+        // б) а 1 19 b
+        Console.WriteLine("а 1 19 b");
+
+        // в) x y 5 y
+        Console.WriteLine("x y 5 y");
+    }
+}
