37864f7c21c63e1e6e31ba53b4cdbaa1bf927aa1
Durak2000 <fetisovtimur0@gmail.com>
2025-10-16 10:02:57 +0300

    vlad s

---
+++ block1/task27/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите трехзначное число: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        if (number < 100 || number > 999)
+        {
+            Console.WriteLine("Ошибка! Число должно быть трехзначным.");
+            return;
+        }
+        
+
+        int firstDigit = number / 100;
+        int lastDigit = number % 10;
+        
+
+        if (firstDigit == lastDigit)
+        {
+            Console.WriteLine($"Число {number} является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number} НЕ является палиндромом.");
+        }
+    }
+}
