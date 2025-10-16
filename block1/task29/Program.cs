c30a7f91f88b2f5f81d4c13217a9fbb50b3db82a
Durak2000 <fetisovtimur0@gmail.com>
2025-10-16 11:57:50 +0300

    кирилл

---
+++ block1/task29/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = 123;
+int digit1 = number / 100;
+int digit2 = (number / 10) % 10;
+int digit3 = number % 10;
+
+long square = (long)number * number; 
+long sumOfCubes = (long)Math.Pow(digit1, 3) + Math.Pow(digit2, 3) + Math.Pow(digit3, 3);
+
+if (square == sumOfCubes)
+{
+    Console.WriteLine($"Квадрат числа {number} равен сумме кубов его цифр.");
+}
+else
+{
+    Console.WriteLine($"Квадрат числа {number} НЕ равен сумме кубов его цифр.");
+}
