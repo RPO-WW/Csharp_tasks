6bf3f3e724d0e2954157ba47bf1b4e61b84b62be
Durak2000 <fetisovtimur0@gmail.com>
2025-10-17 10:18:45 +0300

    кирилл

---
+++ Block2/task21/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class NumberParts
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите натуральное число n (n > 9):");
+        int n = int.Parse(Console.ReadLine());
+
+        int units = n % 10;
+        Console.WriteLine($"а) Число единиц: {units}");
+
+  
+        int tens = (n / 10) % 10;
+        Console.WriteLine($"б) Число десятков: {tens}");
+    }
+}
