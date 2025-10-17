b97610ae1770947fb1452b4062039703768a4d82
Durak2000 <fetisovtimur0@gmail.com>
2025-10-17 10:33:42 +0300

    fetisov

---
+++ Block2/task22/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class NumberParts
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите натуральное число n (n > 99):");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n > 99)
+    {
+        int units = n / 10;
+        Console.WriteLine($"а) Число десятков: {units}");
+
+
+        int tens = n / 100;
+        Console.WriteLine($"б) Число сотен: {tens}");
+    }
+    else
+    {
+        Console.WriteLine("Число должно быть больше 99");
+    }
+    }
+}
