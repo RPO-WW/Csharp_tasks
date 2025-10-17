88bf27696835394919d4c4ed335cb2f48a514d85
Durak2000 <fetisovtimur0@gmail.com>
2025-10-17 11:49:52 +0300

    linda

---
+++ Block2/task24/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Programm
+{
+    static void Main()
+    {
+        Console.WriteLine("введите массу в кг: ");
+        double kg = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"полных центнеров {(int)(kg / 100)}");
+    }
+}
