99cadc4a5eba3886fff977cd3845bfcd38b53fe2
Kris465 <nataza7394@gmail.com>
2025-10-21 11:49:26 +0300

    popov

---
+++ Block2/task28/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        for (int x = 100; x <= 999; x++)
+        {
+            int a = x / 100;
+            int b = (x / 10) % 10; 
+            int c = x % 10;   
+
+
+            int ac = 10 * a + c;
+
+
+            int newNumber = 100 * b + ac;
+
+            if (newNumber == 546)
+            {
+                Console.WriteLine($"Искомое число x: {x}");
+                break;
+            }
+        }
+    }
+}
