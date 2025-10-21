be24ca92965035c5c99ee66a80ccf60f900d103f
Kris465 <nataza7394@gmail.com>
2025-10-21 10:21:31 +0300

    Манираки

---
+++ Block2/task21/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите натуральное число n (n > 9): ");
+int n = Convert.ToInt32(Console.ReadLine());
+int ed = n % 10;
+int des = (n / 10) % 10;
+Console.WriteLine($"а) Число единиц: {ed}");
+Console.WriteLine($"б) Число десятков: {des}");
