07f04a682b9cad96ae4e338d49ed1d0e37f415de
Kris465 <nataza7394@gmail.com>
2025-10-23 11:47:04 +0300

    Манираки

---
+++ Block2/task29/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число n (10 <= n <= 999, десятки ≠ 0): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n < 10 || n > 999) {
+    Console.WriteLine("Ошибка: n должно быть от 10 до 999.");
+    return;
+}
+
+int n1 = n / 100;
+int n2 = (n / 10) % 10;
+int n3 = n % 10;
+
+if(n2 == 0) {
+    Console.WriteLine("Ошибка: цифра десятков в n не должна быть нулём.");
+    return;
+}
+
+int x = 100 * n2 + 10 * n1 + n3;
+
+Console.WriteLine($"Исходное число x: {x}");
