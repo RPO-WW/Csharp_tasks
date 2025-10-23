c35690a7ede4be4f4677e284fc20006b21826ede
Kris465 <nataza7394@gmail.com>
2025-10-23 12:16:39 +0300

    ksusha

---
+++ Block2/task30/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int x = 100; x <= 999; x++)
+        {
+            int a = x / 100;
+            int b = (x / 10) % 10;
+            int c = x % 10;
+
+            int result = (10 * a + c) * 10 + b;
+
+            if (result == 456)
+            {
+                Console.WriteLine($"Искомое число x = {x}");
+                break;
+            }
+        }
+    }
+}
