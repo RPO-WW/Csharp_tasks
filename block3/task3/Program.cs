175bea110cf934852b2aa46a45115c3ee1e7ecd4
Kris465 <nataza7394@gmail.com>
2025-10-24 15:24:02 +0300

    ksusha

---
+++ block3/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool A = true;
+        bool B = false;
+        bool C = false;
+        
+        Console.WriteLine("Дано:");
+        Console.WriteLine($"A = {A}");
+        Console.WriteLine($"B = {B}");
+        Console.WriteLine($"C = {C}");
+        Console.WriteLine();
+        
+        bool resultA = !A && B;
+        Console.WriteLine($"а) не А и В = !{A} && {B} = {resultA}");
+        
+        bool resultB = A || !B;
+        Console.WriteLine($"б) А или не В = {A} || !{B} = {resultB}");
+        
+        bool resultC = A && B || C;
+        Console.WriteLine($"в) А и В или С = {A} && {B} || {C} = {resultC}");
+    }
+}
