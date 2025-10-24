88ca5ed81b71023084cef4c753a1cd1d00cd9ae2
Kris465 <nataza7394@gmail.com>
2025-10-24 15:17:47 +0300

    vita

---
+++ block3/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Заданные значения
+        bool X = false;
+        bool Y = true;
+        bool Z = false;
+
+        Console.WriteLine("Дано:");
+        Console.WriteLine($"X = {X}");
+        Console.WriteLine($"Y = {Y}");
+        Console.WriteLine($"Z = {Z}");
+        Console.WriteLine();
+
+        // а) X или Z
+        bool resultA = X || Z;
+        Console.WriteLine($"а) X или Z = {X} || {Z} = {resultA}");
+
+        // б) X и Y
+        bool resultB = X && Y;
+        Console.WriteLine($"б) X и Y = {X} && {Y} = {resultB}");
+
+        // в) X и Z
+        bool resultC = X && Z;
+        Console.WriteLine($"в) X и Z = {X} && {Z} = {resultC}");
+    }
+}
+
