a165224939cb1a250d52671f00e50383a8ea8b4b
Kris465 <nataza7394@gmail.com>
2025-10-24 11:57:24 +0300

    vlad s

---
+++ Block2/task32/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        int c = 6;
+        int b = 5;
+        int a = 4;
+
+        int x = 100 * a + 10 * b + c;
+        Console.WriteLine($"Искомое число x: {x}");
+    }
+}
