4c9ff84490cbbf7a4c0e71f4858d4479ebd3593b
Kris465 <nataza7394@gmail.com>
2025-10-24 11:38:01 +0300

    Манираки

---
+++ Block2/task31/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите n (100 ≤ n ≤ 999): ");
+        int n = int.Parse(Console.ReadLine());
+        if (n < 100 || n > 999)
+        {
+            Console.WriteLine("Ошибка: n должно быть трёхзначным числом.");
+            return;
+        }
+
+        int a = n / 100;
+        int c = (n / 10) % 10;
+        int b = n % 10;
+
+        int x = 100 * a + 10 * b + c;
+
+        Console.WriteLine($"Исходное число x: {x}");
+    }
+}
