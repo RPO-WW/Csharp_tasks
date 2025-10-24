6613d1c20d117fb92c3b7e8fafc8deebe2222d7c
Kris465 <nataza7394@gmail.com>
2025-10-24 13:30:06 +0300

    Манираки

---
+++ Block2/task37/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите k (1 <= k <= 180): ");
+int k = int.Parse(Console.ReadLine());
+int pairNumber = FindPairNumber(k);
+Console.WriteLine($"а) Номер пары цифр: {pairNumber}");
+int twoDigitNumber = FindTwoDigitNumber(k);
+Console.WriteLine($"б) Двузначное число: {twoDigitNumber}");
+
+Console.WriteLine($"в) k-я цифра:");
+Console.WriteLine($"   Если k четное: {FindKthDigitEven(k)}");
+Console.WriteLine($"   Если k нечетное: {FindKthDigitOdd(k)}");
+
+    static int FindPairNumber(int k)
+    {
+        return (k + 1) / 2;
+    }
+    static int FindTwoDigitNumber(int k)
+    {
+        int pairNumber = FindPairNumber(k);
+        return 9 + pairNumber;
+    }
+    static int FindKthDigitEven(int k)
+    {
+        int twoDigitNumber = FindTwoDigitNumber(k);
+        return twoDigitNumber % 10;
+    }
+    static int FindKthDigitOdd(int k)
+    {
+        int twoDigitNumber = FindTwoDigitNumber(k);
+        return twoDigitNumber / 10;
+    }
