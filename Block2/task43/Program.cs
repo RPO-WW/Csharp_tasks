160684a0c3e5f66f3097a7c3e556f57735ed47bc
Kris465 <nataza7394@gmail.com>
2025-10-24 14:58:25 +0300

    vlad s

---
+++ Block2/task43/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число a: ");
+        int a = int.Parse(Console.ReadLine());
+        
+        Console.Write("Введите число b: ");
+        int b = int.Parse(Console.ReadLine());
+
+
+        
+        bool divisible1 = (b != 0) && (a % b == 0);
+        bool divisible2 = (a != 0) && (b % a == 0);
+        bool zeroCase = (a == 0 && b != 0) || (b == 0 && a != 0);
+        int result = Convert.ToInt32(divisible1 || divisible2 || zeroCase);
+        
+        Console.WriteLine($"Результат: {result}");
+    }
+}
