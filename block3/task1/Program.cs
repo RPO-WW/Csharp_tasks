c0edc1257f16fdcdef296c617eed29246a9004b6
Kris465 <nataza7394@gmail.com>
2025-10-24 15:03:14 +0300

    popov

---
+++ block3/task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        bool A = true;
+        bool B = false;
+        bool C = false;
+        
+        Console.WriteLine("Дано:");
+        Console.WriteLine($"A = {A}");
+        Console.WriteLine($"B = {B}");
+        Console.WriteLine($"C = {C}");
+        Console.WriteLine();
+        
+
+        bool resultA = A || B;
+        Console.WriteLine($"а) А или В = {A} || {B} = {resultA}");
+        
+
+        bool resultB = A && B;
+        Console.WriteLine($"б) А и В = {A} && {B} = {resultB}");
+        
+
+        bool resultC = B || C;
+        Console.WriteLine($"в) В или С = {B} || {C} = {resultC}");
+    }
+}
