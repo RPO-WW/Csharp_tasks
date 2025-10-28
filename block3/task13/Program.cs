cf8cb5fa95375544a212905609644dd7cf908045
Kris465 <nataza7394@gmail.com>
2025-10-28 11:33:02 +0300

    popov

--- block3/task13/Program.cs
+++ block3/task13/Program.cs
@@ -6,28 +6,28 @@ class Program
     {
         Console.WriteLine("ТАБЛИЦА ИСТИННОСТИ ЛОГИЧЕСКИХ ВЫРАЖЕНИЙ");
         Console.WriteLine("=======================================\n");
-        
+
 
         Console.WriteLine("|  A  |  B  | не (A и B) | не A или B | A или не B |");
         Console.WriteLine("|-----|-----|------------|------------|------------|");
-        
+
 
         bool[] boolValues = { false, true };
-        
+
         foreach (bool A in boolValues)
         {
             foreach (bool B in boolValues)
             {
 
                 bool expr1 = !(A && B);
-                bool expr2 = !A || B;    
-                bool expr3 = A || !B;    
-                
+                bool expr2 = !A || B;
+                bool expr3 = A || !B;
+
 
                 Console.WriteLine($"| {A,5} | {B,5} | {expr1,10} | {expr2,10} | {expr3,10} |");
             }
         }
-        
+
 
         Console.WriteLine("\nЗАКОНЫ ДЕ МОРГАНА:");
         Console.WriteLine("==================");
@@ -35,3 +35,4 @@ class Program
         Console.WriteLine("не (А или В) ≡ не А и не В");
     }
 }
+// привет
