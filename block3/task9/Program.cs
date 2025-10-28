889e71090bb9c549a18a800b32694d111cab0660
Kris465 <nataza7394@gmail.com>
2025-10-28 10:12:19 +0300

    Манираки

---
+++ block3/task9/Program.cs
@@ -0,0 +1,6 @@
+﻿bool x = true;
+bool y = false;
+bool z = false;
+Console.WriteLine($"а) {!x || !y || !z}");
+Console.WriteLine($"б) {(!x || !y) && (x || y)}");
+Console.WriteLine($"в) {x && y || x && z || !z}");
