de063df829f6cc4f101e864c2c0a6e764f3cb85d
Kris465 <nataza7394@gmail.com>
2025-10-29 15:20:46 +0300

    ksusha

---
+++ block4/task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Определение области точки");
+        Console.WriteLine("Введите координаты точки (x, y):");
+        
+        Console.Write("x = ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("y = ");
+        double y = Convert.ToDouble(Console.ReadLine());
+        
+        double distanceSquared = x * x + y * y;
+        double radiusSquared = 16;
+        if (distanceSquared <= radiusSquared)
+        {
+            Console.WriteLine($"Точка ({x}, {y}) попадает в область I (внутри круга)");
+        }
+        else
+        {
+            Console.WriteLine($"Точка ({x}, {y}) попадает в область II (вне круга)");
+        }
+    }
+}
