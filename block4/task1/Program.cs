85f8384701cdbfbd2078df46d832345d1f9993e8
Kris465 <nataza7394@gmail.com>
2025-10-29 15:05:11 +0300

    popov

---
+++ block4/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение x:");
+        double x = Convert.ToDouble(Console.ReadLine());
+        
+        double y;
+        
+        if (x > 0)
+        {
+            y = Math.Pow(Math.Sin(x), 2);
+        }
+        else
+        {
+            y = 1 - 2 * Math.Sin(Math.Pow(x, 2));
+        }
+        
+        Console.WriteLine($"При x = {x}");
+        Console.WriteLine($"y = {y}");
+        Console.WriteLine($"y = {y:F4} (с округлением до 4 знаков)");
+    }
+}
