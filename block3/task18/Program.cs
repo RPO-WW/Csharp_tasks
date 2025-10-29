e2ffdcce509756ef7d5c5b491a98c833984b4056
Kris465 <nataza7394@gmail.com>
2025-10-29 09:34:11 +0300

    Манираки

---
+++ block3/task18/Program.cs
@@ -0,0 +1,19 @@
+﻿bool x0 = true;
+bool x1 = false;
+bool y0 = true;
+bool y1 = false;
+
+Console.WriteLine($"а) {!(x0 && !y0) || x0}");
+Console.WriteLine($"а) {!(x1 && !y0) || x1}");
+Console.WriteLine($"а) {!(x0 && !y1) || x0}");
+Console.WriteLine($"а) {!(x1 && !y1) || x1}");
+
+Console.WriteLine($"б) {y0 && !x0 || !y0}");
+Console.WriteLine($"б) {y1 && !x0 || !y1}");
+Console.WriteLine($"б) {y0 && !x1 || !y0}");
+Console.WriteLine($"б) {y1 && !x1 || !y1}");
+
+Console.WriteLine($"в) {!y0 && x0 || y0}");
+Console.WriteLine($"в) {!y1 && x0 || y1}");
+Console.WriteLine($"в) {!y0 && x1 || y0}");
+Console.WriteLine($"в) {!y1 && x1 || y1}");
